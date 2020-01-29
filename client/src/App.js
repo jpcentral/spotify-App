0fdc1a4bced0fca60c4b5c1d7bf67c196b38306d
James Panaho <24538454+jpcentral@users.noreply.github.com>
2020-01-30 11:57:35 +1300

    start of spotify servers

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <a href='http://localhost:8888'>
+<button>Login with Spotify</button>
+</a>
+    </div>
+  );
+}
+
+export default App;
