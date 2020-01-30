34335bc07816e9452e8d3359dc1a4340ba492a91
James Panaho <24538454+jpcentral@users.noreply.github.com>
2020-01-30 14:04:03 +1300

    working build 1

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+constructor() {
+super()
+const params = this.getHashParams()
+
+this.state = {
+  loggedIn: params.access_token ? true : false,
+  playlists: {
+name: 'Not Checked',
+image: ''
+  }
+}
+
+getPlaylist() {
+  
+}
+
+}
+}
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
+  render() {
   return (
     <div className="App">
       <a href='http://localhost:8888'>
 <button>Login with Spotify</button>
 </a>
+  <div>Your Playlists: {this.StaticRange.usersPlaylists.image} {this.StaticRange.playlists.playlist}
+<button onClick={() => getPlaylist()}>Get Playlists
+</button>
+  </div>
     </div>
   );
 }
+
 
 export default App;
